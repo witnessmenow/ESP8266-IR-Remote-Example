5f4ead4bf7031720c1a4b57612f06680a757b648
witnessmenow <ecenuig@gmail.com>
2017-02-19 00:27:25 +0000

    Wokring example with basic readme

---
+++ BasicWebRemote/BasicWebRemote.ino
@@ -0,0 +1,174 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+// Add this library: https://github.com/markszabo/IRremoteESP8266
+#include <IRremoteESP8266.h>
+
+#define IR_SEND_PIN D2
+
+IRsend irsend(IR_SEND_PIN);
+
+const char* ssid = "SSID";
+const char* password = "password";
+
+ESP8266WebServer server(80);
+
+const int led = BUILTIN_LED;
+
+String rowDiv = "    <div class=\"row\" style=\"padding-bottom:1em\">\n";
+String endDiv = "    </div>\n";
+
+String generateButton(String colSize, String id, String text, String url) {
+
+  return  "<div class=\"" + colSize + "\" style=\"text-align: center\">\n" +
+          "    <button id=\"" + id + "\" type=\"button\" class=\"btn btn-default\" style=\"width: 100%\" onclick='makeAjaxCall(\"" + url + "\")'>" + text + "</button>\n" +
+          "</div>\n";
+}
+
+void handleRoot() {
+  digitalWrite(led, 0);
+  String website = "<!DOCTYPE html>\n";
+  website = website + "<html>\n";
+  website = website + "  <head>\n";
+  website = website + "    <meta charset=\"utf-8\">\n";
+  website = website + "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
+  website = website + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+  website = website + "    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n";
+  website = website + "  </head>\n";
+  website = website + "  <body>\n";
+  website = website + "    <div class=\"container-fluid\">\n";
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-6", "tvpower","TV Power", "tvpower");
+  website = website + generateButton("col-xs-6", "sspower","SS Power", "sspower");
+  website = website + endDiv;
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-3", "sschannel1","PS4", "sschannel1");
+  website = website + generateButton("col-xs-3", "sschannel2","FTV", "sschannel2");
+  website = website + generateButton("col-xs-3", "sschannel3","---", "sschannel3");
+  website = website + generateButton("col-xs-3", "sschannel4","SAT", "sschannel4");
+  website = website + endDiv;
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-12", "up","Vol Up", "up");
+  website = website + endDiv;
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-12", "down","Vol Down", "down");
+  website = website + endDiv;
+  website = website + endDiv;
+  website = website + "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>\n";
+  website = website + "    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n";
+  website = website + "    <script> function makeAjaxCall(url){$.ajax({\"url\": url})}</script>\n";
+  website = website + "  </body>\n";
+  website = website + "</html>\n";
+
+  server.send(200, "text/html", website);
+  digitalWrite(led, 1);
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 1);
+}
+
+void setup(void){
+  irsend.begin();
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 1);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/down", [](){
+    Serial.println("Sorround Sound Down");
+    irsend.sendNEC(0x4BB6C03F, 32);
+    server.send(200, "text/plain", "Volume Down");
+  });
+
+  server.on("/up", [](){
+    Serial.println("Sorround Sound Up");
+    irsend.sendNEC(0x4BB640BF, 32);
+    server.send(200, "text/plain", "Volume Up");
+  });
+
+  server.on("/sspower", [](){
+    Serial.println("Sorround Sound power");
+    irsend.sendNEC(0x4B3420DF, 32);
+    server.send(200, "text/plain", "Sorround Sound power");
+  });
+
+  server.on("/sschannel1", [](){
+    Serial.println("Sorround Sound Channel 1");
+    irsend.sendNEC(0x4B3631CE, 32);
+    server.send(200, "text/plain", "Sorround Sound Channel 1");
+  });
+
+  server.on("/sschannel2", [](){
+    Serial.println("Sorround Sound Channel 2");
+    irsend.sendNEC(0x4BB6F00F, 32);
+    server.send(200, "text/plain", "Sorround Sound Channel 2");
+  });
+
+  server.on("/sschannel3", [](){
+    Serial.println("Sorround Sound Channel 3");
+    irsend.sendNEC(0x4BB6708F, 32);
+    server.send(200, "text/plain", "Sorround Sound Channel 3");
+  });
+
+  server.on("/sschannel4", [](){
+    Serial.println("Sorround Sound Channel 4");
+    irsend.sendNEC(0x4BB6B04F, 32);
+    server.send(200, "text/plain", "Sorround Sound Channel 4");
+  });
+
+  server.on("/sstvsound", [](){
+    Serial.println("Sorround Sound sstvsound");
+    irsend.sendNEC(0x4BB6906F, 32);
+    server.send(200, "text/plain", "Sorround Sound sstvsound");
+  });
+
+  server.on("/tvpower", [](){
+    Serial.println("TV power");
+    irsend.sendNEC(0x20DF10EF, 32);
+    server.send(200, "text/plain", "TV power");
+  });
+
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ESP8266-IR-Remote-Example
+
+This sketch is an example of using an ESP8266 as a IR sender controlled by a webpage.
+
+It uses the [IRremoteESP8266 library by markszabo](https://github.com/markszabo/IRremoteESP8266).
+
+This example is the codes for my devices, in order for it to work for you will need to program in your devices codes. I use the [DumpV2 example](https://github.com/markszabo/IRremoteESP8266/blob/master/examples/IRrecvDumpV2/IRrecvDumpV2.ino) in the IRremoteESP8266 library.
+
+## Hardware
+
+- Wemos D1 Mini
+- IR LED
+- 2N3904 Transistor
+- 330 Ohm resistor
+- IR receiver (TL1838) - Needed for reading codes
+
+![alt text](http://i.imgur.com/druTsOU.png "circuit")
